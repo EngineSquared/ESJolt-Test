4d6864884d59a777934be16456c92a26a50c5084
lucas <lucas.hauszler@gmail.com>
2025-03-14 18:36:37 +0100

    refactor: integrate sphere as ecs entity as well

--- src/main.cpp
+++ src/main.cpp
@@ -22,6 +22,56 @@ using namespace JPH; // NOT RECOMMENDED
 using namespace JPH::literals;
 using namespace ES::Plugin::Physics;
 
+class InitSphereSystem
+{
+public:
+	ES::Engine::Entity sphereEntity;
+	mutable bool gotVelocity = false;
+
+	void operator()(ES::Engine::Core &core) const
+	{
+		if (gotVelocity)
+		{
+			return;
+		}
+
+		auto &rigidBody = core.GetRegistry().get<ES::Plugin::Physics::Component::RigidBody3D>(sphereEntity);
+
+		if (rigidBody.body == nullptr)
+		{
+			return;
+		}
+
+		auto &physicsManager = core.GetResource<ES::Plugin::Physics::Resource::PhysicsManager>();
+		auto &physicsSystem = physicsManager.GetPhysicsSystem();
+		auto &bodyInterface = physicsSystem.GetBodyInterface();
+
+		bodyInterface.SetLinearVelocity(rigidBody.body->GetID(), Vec3(0.0f, -5.0f, 0.0f));
+
+		gotVelocity = true;
+	}
+};
+
+class PrintSphereInfoSystem 
+{
+public:
+	ES::Engine::Entity sphereEntity;
+
+	void operator()(ES::Engine::Core &core) const
+	{
+		auto &rigidBody = core.GetRegistry().get<ES::Plugin::Physics::Component::RigidBody3D>(sphereEntity);
+
+		auto &physicsManager = core.GetResource<ES::Plugin::Physics::Resource::PhysicsManager>();
+		auto &physicsSystem = physicsManager.GetPhysicsSystem();
+		auto &bodyInterface = physicsSystem.GetBodyInterface();
+
+		RVec3 position = bodyInterface.GetCenterOfMassPosition(rigidBody.body->GetID());
+		Vec3 velocity = bodyInterface.GetLinearVelocity(rigidBody.body->GetID());
+
+		std::cout << "Position = (" << position.GetX() << ", " << position.GetY() << ", " << position.GetZ() << "), Velocity = (" << velocity.GetX() << ", " << velocity.GetY() << ", " << velocity.GetZ() << ")" << std::endl;
+	}
+};
+
 int main(void)
 {
     ES::Engine::Core core;
@@ -49,16 +99,16 @@ int main(void)
 	floor.AddComponent<ES::Plugin::Physics::Component::RigidBody3D>(core, ES::Plugin::Physics::Component::RigidBody3D(floor_shape_settings, EMotionType::Static, Utils::Layers::NON_MOVING));
 
 	// Now create a dynamic body to bounce on the floor
-	// Note that this uses the shorthand version of creating and adding a body to the world
-	BodyCreationSettings sphere_settings(new SphereShape(0.5f), RVec3(0.0_r, 2.0_r, 0.0_r), Quat::sIdentity(), EMotionType::Dynamic, Utils::Layers::MOVING);
-	BodyID sphere_id = body_interface.CreateAndAddBody(sphere_settings, EActivation::Activate);
+	std::shared_ptr<SphereShapeSettings> sphere_shape_settings = std::make_shared<SphereShapeSettings>(0.5f);
 
-	// Now you can interact with the dynamic body, in this case we're going to give it a velocity.
-	// (note that if we had used CreateBody then we could have set the velocity straight on the body before adding it to the physics system)
-	body_interface.SetLinearVelocity(sphere_id, Vec3(0.0f, -5.0f, 0.0f));
+	ES::Engine::Entity sphere = core.CreateEntity();
+	sphere.AddComponent<ES::Plugin::Object::Component::Transform>(core, ES::Plugin::Object::Component::Transform({0.0f, 2.0f, 0.0f}));
+	sphere.AddComponent<ES::Plugin::Physics::Component::RigidBody3D>(core, ES::Plugin::Physics::Component::RigidBody3D(sphere_shape_settings, EMotionType::Dynamic, Utils::Layers::MOVING));
 
-	// We simulate the physics world in discrete time steps. 60 Hz is a good rate to update the physics system.
-	const float cDeltaTime = 1.0f / 60.0f;
+	// Now that we know which entity is the sphere, we can create its linked system
+	// Note that this is for testing purposes only
+	core.RegisterSystem(InitSphereSystem{ sphere });
+	core.RegisterSystem(PrintSphereInfoSystem{ sphere });
 
 	// Optional step: Before starting the physics simulation you can optimize the broad phase. This improves collision detection performance (it's pointless here because we only have 2 bodies).
 	// You should definitely not call this every frame or when e.g. streaming in a new level section as it is an expensive operation.
@@ -66,32 +116,14 @@ int main(void)
 	physics_system.OptimizeBroadPhase();
 
 	// Now we're ready to simulate the body, keep simulating until it goes to sleep
-	uint step = 0;
-	while (body_interface.IsActive(sphere_id))
+	while (true)
 	{
-		// Next step
-		++step;
-
-		// Output current position and velocity of the sphere
-		RVec3 position = body_interface.GetCenterOfMassPosition(sphere_id);
-		Vec3 velocity = body_interface.GetLinearVelocity(sphere_id);
-		std::cout << "Step " << step << ": Position = (" << position.GetX() << ", " << position.GetY() << ", " << position.GetZ() << "), Velocity = (" << velocity.GetX() << ", " << velocity.GetY() << ", " << velocity.GetZ() << ")" << std::endl;
-
-		// If you take larger steps than 1 / 60th of a second you need to do multiple collision steps in order to keep the simulation stable. Do 1 collision step per 1 / 60th of a second (round up).
-		const int cCollisionSteps = 1;
-
 		// Step the world
 		core.RunSystems();
 
 		// Simulate a framerate of 60 Hz
 		std::this_thread::sleep_for(std::chrono::duration<float>(1/60.0f));
 	}
-
-	// Remove the sphere from the physics system. Note that the sphere itself keeps all of its state and can be re-added at any time.
-	body_interface.RemoveBody(sphere_id);
-
-	// Destroy the sphere. After this the sphere ID is no longer valid.
-	body_interface.DestroyBody(sphere_id);
 
     return 0;
 }
